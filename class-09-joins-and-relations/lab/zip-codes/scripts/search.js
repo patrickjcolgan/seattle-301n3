a2deb0833be803728f81aa72552e78d32967f5b6
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-06-26 12:27:19 -0700

    cities populate

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -4,7 +4,7 @@ var Filters = {};
 
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
-  Filters.getStates = function(){
+  Filters.getStates = function() {
     // if(zips) {
       webDB.execute('SELECT DISTINCT state from zips ORDER BY state ASC', function(rows) {
         var statesArr = [];
@@ -16,7 +16,34 @@ var Filters = {};
           $('#state-select').append(optionTag);
         })
       })
+      Filters.setCity();
     // })
+  };
+
+  Filters.setCity = function () {
+  var result;
+  $('#state-select').on('change', function() {
+    console.log("on change");
+    $("#city-select").empty();
+    $("#city-select").append('<option value="city">Select a City</option>');
+    result = "'" + $(this).val() + "'";
+    console.log(result);
+    Filters.populateCity(result);
+  })
+}
+
+  Filters.populateCity = function(ele) {
+    webDB.execute('SELECT DISTINCT city FROM zips WHERE state = ' + ele + 'ORDER BY city ASC;', function(cities) {
+      console.log('cities fired');
+      var cityArr = [];
+      cities.map(function(a) {
+        cityArr.push(a.city);
+      })
+      cityArr.forEach(function(name) {
+        $('#city-select').append('<option value="' + name + '">' + name + '</option>');
+      })
+    })
   }
+
 module.Filters = Filters;
 })(window);
