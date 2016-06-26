eeeaec62739205517f76e8637a91821c52869a77
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-06-26 16:13:12 -0700

    pins drop zip and state selection

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -1,10 +1,27 @@
-function initMap() {
+function initMap(dataArray) {
   // Create a map object and specify the DOM element for display.
+  if(dataArray){
+      var customMap = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: dataArray[0].latitude, lng: dataArray[0].longitude},
+        scrollwheel: true,
+        zoom: 8
+      });
+      
+      dataArray.forEach(function(ele) {
+        //make markers
+        var marker = new google.maps.Marker({
+           position: {lat: ele.latitude, lng: ele.longitude},
+           map: customMap
+         });
+      })
+} else {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.611435, lng: -122.330456},
     scrollwheel: true,
     zoom: 8
   });
 
+
+}
   // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
 }
--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -2,6 +2,8 @@
 
 var Filters = {};
 var ZipForm = {};
+var MapMark = {};
+var coordinates;
 
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
@@ -31,7 +33,8 @@ var ZipForm = {};
     console.log(result);
     Filters.populateCity(result);
   })
-}
+  Filters.cityMarker();
+};
 
   Filters.populateCity = function(ele) {
     webDB.execute('SELECT DISTINCT city FROM zips WHERE state = ' + ele + 'ORDER BY city ASC;', function(cities) {
@@ -44,24 +47,38 @@ var ZipForm = {};
         $('#city-select').append('<option value="' + name + '">' + name + '</option>');
       })
     })
-  }
+  };
+
+  Filters.cityMarker = function (ele) {
+  $('#city-select').on('change', function(e) {
+    e.preventDefault();
+    var cityDropDown = e.target.value;
+    var stateDropDown = $('#state-select').val();
+    console.log(stateDropDown);
+    console.log(cityDropDown);
+    webDB.execute(
+      'SELECT DISTINCT latitude, longitude, city, state FROM zips WHERE city = '+ '"' + cityDropDown + '"' + ' AND state =' +'"' + stateDropDown + '"' + ';',
+      function(param) {
+      console.log(param);
+      initMap(param);
+    });
+  });
+  };
+
 
  ZipForm.zipInput = function() {
    $('form').on('submit', function(e) {
      e.preventDefault();
      var $zipSearch = $('.zip-search').val();
-     webDB.execute('SELECT DISTINCT city, zip FROM zips WHERE zip = ' + $zipSearch + ';', function(rows) {
-       rows.forEach(function(ele) {
-         console.log(ele);
-       })
-       if (rows.length >= 1) {
-         console.log("zip input successful");
-       } else {
-         console.log("zip input failure!");
-       }
-     });
- })
- };
+     webDB.execute(
+       'SELECT DISTINCT latitude, longitude, city, zip FROM zips WHERE zip = ' + $zipSearch + ';',
+       function(data) {
+         console.log(data);
+        initMap(data);
+       });
+});
+};
+
 
 ZipForm.zipInput();
 module.Filters = Filters;
