97a85fd413c4e547a93a219d66688bb49458ab25
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-07-06 15:07:20 -0700

    repo.js and Handlebar.compile

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
@@ -5,8 +5,9 @@
     // REVIEW: Look at this method chaning. What is being accomplished here?
     $('#about').show().siblings().hide();
 
-    // TODO: Call a function to 'request' our repo data.
+    // DONE TODO: Call a function to 'request' our repo data.
     // Pass in a view function as a callback, so our repos will render after the data is loaded.
+    repos.requestRepos(repoView.index);
   };
 
   module.aboutController = aboutController;
--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -4,8 +4,16 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
+    // DONE?? TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/users/patrickjcolgan/repos?per_page=10&sorted=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        repos.all = data;
+        callback(data);
+      }
+    });
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
